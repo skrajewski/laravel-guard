f2c24029aa06eec4e16f82d4aa0e57c23c848a25
Szymon Krajewski <szykra@gmail.com>
2015-05-11 21:56:17 +0200

    Develop eloquent adapter

--- src/Szykra/Guard/Adapters/EloquentAdapter.php
+++ src/Szykra/Guard/Adapters/EloquentAdapter.php
@@ -9,18 +9,68 @@ class EloquentAdapter implements PermissionChecker {
 
     protected static $permissions = [];
 
+    protected $roleObj;
+
+    public function __construct($className)
+    {
+        $this->roleObj = new $className;
+    }
+
+    /**
+     * Checks if role has permission
+     *
+     * @param $role
+     * @param $permission
+     * @return mixed
+     */
     public function roleHasPermission($role, $permission)
     {
-        if(array_key_exists($role, static::$permissions)) {
+        if($this->permissionsAreLoaded($role)) {
             $roleModel = static::$permissions[$role];
         } else {
-            $roleModel = Role::with("permissions")->where("tag", "=", $role)->firstOrFail();
+            $roleModel = $this->getRole($role);
+
             static::$permissions[$role] = $roleModel;
         }
 
-        return $roleModel->permissions->filter(function($element) use($permission) {
+        return $this->permissionExists($roleModel, $permission);
+    }
+
+    /**
+     * Checks if role is currently loaded
+     *
+     * @param $role
+     * @return bool
+     */
+    private function permissionsAreLoaded($role)
+    {
+        return array_key_exists($role, static::$permissions);
+    }
+
+    /**
+     * Retrieve role from database
+     *
+     * @param $roleName
+     * @return
+     */
+    private function getRole($roleName)
+    {
+        return $this->roleObj->with("permissions")->where("tag", "=", $roleName)->firstOrFail();
+    }
+
+    /**
+     * Check if permission belongs to role
+     * @param $role
+     * @param $permission
+     * @return mixed
+     */
+    private function permissionExists($role, $permission)
+    {
+        $requiredPermission = $role->permissions->filter(function($element) use($permission) {
             return $element->tag === $permission;
         });
+
+        return (bool) $requiredPermission->count();
     }
 
 }
