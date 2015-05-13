05bdc9da958aad9c424d7fb1e94ca7b091dc165d
Dawid Kuśmierek <kusmierekd@gmail.com>
2015-05-13 19:07:38 +0200

    Added exception for missing configuration file

--- src/Szykra/Guard/Adapters/YamlAdapter.php
+++ src/Szykra/Guard/Adapters/YamlAdapter.php
@@ -4,6 +4,7 @@ namespace Szykra\Guard\Adapters;
 
 use Symfony\Component\Yaml\Yaml;
 use Szykra\Guard\Contracts\PermissionChecker;
+use Szykra\Guard\Exceptions\MissingConfigFileException;
 
 class YamlAdapter implements PermissionChecker {
 
@@ -12,7 +13,14 @@ class YamlAdapter implements PermissionChecker {
     public function roleHasPermission($role, $permission)
     {
         if(empty(static::$roles)) {
-            static::$roles = Yaml::parse(file_get_contents(base_path() . '/config/' . config('guard.adapter_config_file')));
+            $configFile = base_path() . '/config/' . config('guard.adapter_config_file');
+
+            if(!is_file($configFile)) {
+                throw new MissingConfigFileException("Missing configuration file.");
+            }
+
+            static::$roles = Yaml::parse(file_get_contents($configFile));
+
         }
 
         return in_array($permission, static::$roles[$role]['permissions']);
