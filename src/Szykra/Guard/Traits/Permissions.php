117d20822949383477932a2bcf07c7890a60ff8e
Szymon Krajewski <szykra@gmail.com>
2015-05-13 20:32:09 +0200

    Removed eloquent specific code from permission trait (not done yet)

--- src/Szykra/Guard/Traits/Permissions.php
+++ src/Szykra/Guard/Traits/Permissions.php
@@ -1,9 +1,9 @@
 <?php namespace Szykra\Guard\Traits;
 
-use Szykra\Guard\Exceptions\MissingRoleRelationException;
-
 trait Permissions
 {
+
+    static protected $permissionCheckerAdapter;
 
     /**
      * Buffer for permissions
@@ -31,38 +31,11 @@ trait Permissions
      */
     public function can($permission)
     {
-        $adapter = \App::make("Szykra\\Guard\\Contracts\\PermissionChecker");
-
-        return $adapter->roleHasPermission($this->role->tag, $permission);
-
-        // if (!$this->permissions) {
-        //     $this->permissions = array_pluck($this->getPermissions(), 'tag');
-        // }
-        // 
-        // return in_array($permission, $this->permissions);
-    }
-
-    /**
-     * Permissions collection
-     *
-     * @return mixed
-     * @throws RoleNotFoundException
-     */
-    private function getPermissions()
-    {
-        return $this->role->permissions;
-    }
-
-    /**
-     * Check if relation exists in model
-     *
-     * @throws RoleNotFoundException
-     */
-    private function checkIfRelationToRoleExist()
-    {
-        if (!method_exists($this, 'role')) {
-            throw new MissingRoleRelationException('Please set relation to role as public role() method.');
+        if (!static::$permissionCheckerAdapter) {
+            static::$permissionCheckerAdapter = \App::make("Szykra\\Guard\\Contracts\\PermissionChecker");
         }
+
+        return static::$permissionCheckerAdapter->roleHasPermission($this->role->tag, $permission);
     }
 
 }
